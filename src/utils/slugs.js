35857b009e9f85304c6053ccd629fa028c02156c
Andrea Crawford <andrea.crawford13@gmail.com>
2019-06-18 12:45:31 +0100

    Fix slugify function

--- src/utils/slugs.js
+++ src/utils/slugs.js
@@ -1,4 +1,5 @@
-export const stringifyChildren = children =>
-  typeof children === 'string'
-    ? children
-    : children.map(val => (typeof val === 'string' ? val : val.props.children)).join('');
+export const stringifyChildren = children => {
+  if (typeof children === 'string') return children;
+  if (children.map) return children.map(val => (typeof val === 'string' ? val : val.props.children)).join('');
+  return children.props.children;
+};
